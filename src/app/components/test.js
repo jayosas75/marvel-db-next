5813c7535ccf43c60ca57ca9ce85b564d5d7b1c2
jayosas75 <jayosas75@gmail.com>
2023-12-08 22:30:11 -0800

    retro-react added

---
+++ src/app/components/test.js
@@ -0,0 +1,40 @@
+'use client'
+import { createContext } from 'react'
+import React from 'react';
+import {
+	Alert,
+	Box,
+	Button,
+	Card,
+	Chip,
+	Container,
+	Input,
+	Modal,
+	ProgressBar,
+	Text,
+} from 'retro-react';
+
+const Context = createContext()
+
+const Test = () => {
+	return (
+		<div>
+			<Button>Click me</Button>
+			<Container>{/* Your content here */}</Container>
+			<Input placeholder="Enter text" />
+			<Box>Box content</Box>
+			<Card header="Header" footer="Footer">
+				{/* Card content */}
+			</Card>
+			<Text variant="h1">Hello, world!</Text>
+			<Modal open={true}>{/* Modal content */}</Modal>
+			<ProgressBar value={50} />
+			<Chip color="primary">Chip</Chip>
+			<Alert open={true} color="success">
+				{/* Alert content */}
+			</Alert>
+		</div>
+	);
+};
+
+export default Test;
--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Test from './components/test'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <Test />
       </div>
 
       <div className={styles.center}>
